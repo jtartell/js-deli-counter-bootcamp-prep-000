8d70d87e8679e6455c85a48068c5d033870bc57a
Jamie Tartell <jetartell@gmail.com>
2020-03-21 15:35:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
+
+
+var katzDeliLine = [];
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+   return `There is nobody waiting to be served!`
+} else {
+   return(`Currently serving ${katzDeliLine.shift()}.`)
+}
+}
+nowServing(katzDeliLine)
+
+var katzDeliLine = [];
+f  for (i=0; i<katzDeliLine.length; )
+   return(`Currently serving ${katzDeliLine}.`)
+}
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+   return `The line is currently empty.`
+} else {
+  for (i=0; i<katzDeliLine.length; )
+   return(`Currently serving ${katzDeliLine}.`)
+}
+}
+
+var katzDeliLine = [];
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+   return `The line is currently empty.`
+} else {
